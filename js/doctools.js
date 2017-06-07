336648d2e01007261ee506d59fac8800035ff2b2
qiaojiange <1967254382@qq.com>
2017-06-07 11:31:21 +0800

    第一次提交所学的效果，详细看showLearn.html

---
+++ js/doctools.js
@@ -0,0 +1,12 @@
+var doc = document;
+function byId(id){
+	return doc.getElementById(id);
+}
+
+function byName(name){
+	return doc.getElementsByName(name);
+}
+
+function byTag(tag){
+	return doc.getElementsByTagName(tag);
+}
